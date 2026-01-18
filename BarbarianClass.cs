d9e506cbddeb5deb0750156a2f709aa3e0f77b38
Aaron <aaroday1@live.com>
2026-01-18 13:30:47 +0000

    namespace ZombieSurvivalTextGame {     public class BarbarianClass     {         public static void ApplyBarbarian()         {             Program.health += 100;             Program.armor += 10;             Program.Attack_Dmg += 10;             Program.accuracy += 10;         }     } }

---
+++ BarbarianClass.cs
@@ -0,0 +1,13 @@
+namespace ZombieSurvivalTextGame
+{
+    public class BarbarianClass
+    {
+        public static void ApplyBarbarian()
+        {
+            Program.health += 100;
+            Program.armor += 10;
+            Program.Attack_Dmg += 10;
+            Program.accuracy += 10;
+        }
+    }
+}
---
+++ Intro.cs
@@ -0,0 +1,46 @@
+namespace ZombieSurvivalTextGame
+{
+    public class Program
+    {
+        public static int health = 100;
+        public static int evasion_Rating = 0;
+        public static int armor = 0;
+        public static int Attack_Dmg = 10;
+        public static int accuracy = 20;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("welcome adventurer!");
+            Console.WriteLine("What is your name?..");
+            string name = Console.ReadLine();
+            Console.WriteLine($"Welcome {name}");
+
+            Console.WriteLine($"Please Choose your class {name}");
+            Console.WriteLine("-----------------------------");
+            Console.WriteLine("Barbarian: \n Health - + 100. \n Damage with Maces & Axes + 10. \n Armor = + 10");
+
+
+
+            Console.WriteLine($"Welcome {name}.");
+            Console.WriteLine($"Your stats:\nName = {name}\nHealth = {health}\nHunger = {hunger}\nEvasion Rating = {evasion_Rating}\nArmor = {armor}");
+
+            Console.WriteLine("A zombie appears!");
+            Console.WriteLine("Do you attack or run? \n Please choose the following: A = Attack or R = Run.");
+
+            string decision1 = Console.ReadLine();
+
+            if (decision1.ToLower() == "a")
+            {
+                Console.WriteLine("Your probability of a successful attack depends on your accuracy rating and the enemies evasion.");
+            }
+            else if (decision1.ToLower() == "r")
+            {
+                Console.WriteLine("You chose to run away");
+            }
+            else
+            {
+                Console.WriteLine("Invalid input");
+            }
+        }
+    }
+}
